00f23cfcfc77da621aeaed18012bb6e3a4575f05
Ludovic Tiako <ludovic.tiako@gmail.com>
2021-05-20 20:55:57 -0400

    Publish bars

--- scribe/platforms/binance.py
+++ scribe/platforms/binance.py
@@ -1,7 +1,10 @@
+import json
 import logging
 import time
+from datetime import datetime, timezone
 from typing import Optional, Sequence, Union
 
+import aioredis
 from binance import AsyncClient, BinanceSocketManager
 from pydantic import validate_arguments
 
@@ -11,14 +14,15 @@ logger = logging.getLogger(__name__)
 
 
 @validate_arguments
-async def listen(
+async def bars(
     coins: Union[str, Sequence[str]],
     *,
+    broker_url: str,
     interval: BinanceKlineInterval = BinanceKlineInterval._1m,
     lifetime: Optional[int] = None,
 ):
     """
-    Opens a websocket to binance and listens for spot, futures and basis prices.
+    Opens a websocket to binance and listens for spot, futures and basis price bars.
 
     Args:
         coins: list of crypto pairs to monitor.
@@ -27,6 +31,7 @@ async def listen(
     """
     if isinstance(coins, str):
         coins = [coins]
+    redis_client = aioredis.from_url(broker_url)
     client = await AsyncClient.create()
     socket_manager = BinanceSocketManager(client)
     socket = socket_manager.multiplex_socket(
@@ -41,7 +46,28 @@ async def listen(
         end_time = (time.time() + lifetime) if lifetime else float("inf")
         while time.time() < end_time:
 
-            res = await socket.recv()
-            logger.debug(res)
+            datastream = await socket.recv()
+            logger.debug(datastream)
+
+            # publish to redis
+            kline = datastream["data"]["k"]
+            bar = {
+                "start_time": datetime.fromtimestamp(
+                    kline["t"] / 1000, timezone.utc
+                ).isoformat(),
+                "end_time": datetime.fromtimestamp(
+                    kline["T"] / 1000, timezone.utc
+                ).isoformat(),
+                "open": kline["o"],
+                "high": kline["h"],
+                "low": kline["l"],
+                "close": kline["c"],
+                "base_volume": kline["v"],
+                "quote_volume": kline["q"],
+                "trades": kline["n"],
+                "is_closed": kline["x"],
+            }
+            channel = f"binance/{kline['s'].lower()}/spot/{kline['i']}"
+            await redis_client.publish(channel, json.dumps(bar))
 
     await client.close_connection()
--- tests/test_binance.py
+++ tests/test_binance.py
@@ -1,17 +1,20 @@
 import logging
 
+import aioredis
 import pytest
 
 import scribe
 
+logger = logging.getLogger(__name__)
+
 
 @pytest.mark.parametrize("coins", ["bnbbtc", ["bnbbtc"], ["btcusdt", "bnbusd"]])
 @pytest.mark.asyncio
-async def test_listen(caplog, coins):
+async def test_listen(caplog, coins, redis_url):
     with caplog.at_level(logging.DEBUG, logger="scribe.platforms.binance"):
-        await scribe.platforms.binance.listen(coins, lifetime=5)
+        await scribe.platforms.binance.bars(coins, broker_url=redis_url, lifetime=5)
 
-        # make sure we logged at least one candlestick
+        # make sure we logged at least one bar
         seen = False
         for record in caplog.records:
             if not isinstance(record.msg, dict):
@@ -21,3 +24,14 @@ async def test_listen(caplog, coins):
             seen = True
             break
         assert seen
+
+
+@pytest.mark.asyncio
+async def test_subscribe(scribe_binance, redis_url):
+    redis_client = aioredis.from_url(redis_url)
+    channel = redis_client.pubsub()
+    await channel.psubscribe("binance/*")
+    async for message in channel.listen():
+        # make sure we receive at least one message
+        if message["type"] == "pmessage":
+            break
